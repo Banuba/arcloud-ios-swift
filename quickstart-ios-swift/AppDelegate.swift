1ff132e41c9081419486454abf35837665f707b5
Andrey Krivoshey <andrey.krivoshey@banuba.com>
2021-02-25 13:15:05 +0300

    Add ARCloud iOS example app

---
+++ quickstart-ios-swift/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import BanubaSdk
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_
+        application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let deviceDocumentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.path
+        BanubaSdkManager.initialize(
+            resourcePath: [Bundle.main.bundlePath + "/effects", deviceDocumentsPath! + "/Effects/"],
+            clientTokenString: banubaClientToken)
+        return true
+    }
+}
---
+++ quickstart-ios-swift/Camera/CameraViewController.swift
@@ -0,0 +1,72 @@
+import UIKit
+import BanubaSdk
+
+class CameraViewController: UIViewController {
+    
+    @IBOutlet weak var effectView: EffectPlayerView!
+    
+    private var sdkManager = BanubaSdkManager()
+    private let config = EffectPlayerConfiguration(renderMode: .video)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        effectView.layoutIfNeeded()
+        sdkManager.setup(configuration: config)
+        setUpRenderSize()
+        effectView?.effectPlayer = sdkManager.effectPlayer
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sdkManager.input.startCamera()
+        _ = sdkManager.loadEffect("UnluckyWitch", synchronous: true)
+        sdkManager.startEffectPlayer()
+    }
+    
+    deinit {
+        sdkManager.destroyEffectPlayer()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        sdkManager.stopEffectPlayer()
+        sdkManager.removeRenderTarget()
+        coordinator.animateAlongsideTransition(in: effectView, animation: { (UIViewControllerTransitionCoordinatorContext) in
+            self.sdkManager.autoRotationEnabled = true
+            self.setUpRenderSize()
+        }, completion: nil)
+    }
+    
+    private func setUpRenderTarget() {
+        guard let effectView = self.effectView.layer as? CAEAGLLayer else { return }
+        sdkManager.setRenderTarget(layer: effectView, playerConfiguration: nil)
+        sdkManager.startEffectPlayer()
+    }
+    
+    private func setUpRenderSize() {
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            config.orientation = .deg90
+            config.renderSize = CGSize(width: 720, height: 1280)
+            sdkManager.autoRotationEnabled = false
+            setUpRenderTarget()
+        case .portraitUpsideDown:
+            config.orientation = .deg270
+            config.renderSize = CGSize(width: 720, height: 1280)
+            setUpRenderTarget()
+        case .landscapeLeft:
+            config.orientation = .deg180
+            config.renderSize = CGSize(width: 1280, height: 720)
+            setUpRenderTarget()
+        case .landscapeRight:
+            config.orientation = .deg0
+            config.renderSize = CGSize(width: 1280, height: 720)
+            setUpRenderTarget()
+        default:
+            setUpRenderTarget()
+        }
+    }
+    
+    @IBAction func closeCamera(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
