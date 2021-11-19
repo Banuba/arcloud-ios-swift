7bcf4fe6aae4693e3571dd4468fd85960a737e89
akrivbanuba <andrey.krivoshey@banuba.com>
2021-11-19 13:41:13 +0300

    Fix build for 1.x

--- arcloud-ios-swift/ARCloud/ARCloudViewController.swift
+++ arcloud-ios-swift/ARCloud/ARCloudViewController.swift
@@ -10,7 +10,7 @@ class ARCloudViewController: UIViewController, UICollectionViewDelegate, UIColle
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     private var sdkManager = BanubaSdkManager()
-    private let config = EffectPlayerConfiguration(renderMode: .video, renderContentMode: .resizeAspectFill)
+    private let config = EffectPlayerConfiguration(renderMode: .video)
     private var effectsArray: [AREffect] = []
     
     //MARK: - ARCloud
