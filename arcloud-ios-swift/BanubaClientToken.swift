5491dfaedc6b271887f912e71dc348d44e18dfe1
akrivbanuba <andrey.krivoshey@banuba.com>
2022-02-09 14:52:16 +0300

    Update AR Cloud URL for 0.x version

--- arcloud-ios-swift/BanubaClientToken.swift
+++ arcloud-ios-swift/BanubaClientToken.swift
@@ -1,4 +1,4 @@
 import Foundation
 
 internal let banubaClientToken: String = <#Place your Face AR token here#>
-internal let banubaArCloudURL: String = "https://api.arcloud.banuba.net/v1/effects/far_test_v1.2" // Bucket with Demo effects, can be replaced by your own AR Cloud URL
+internal let banubaArCloudURL: String = "https://api.arcloud.banuba.net/v1/effects/far_test_v0.38" // Bucket with Demo effects, can be replaced by your own AR Cloud URL
